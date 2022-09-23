cfd4eca9bc2aa857c14977b0d73d2d17596c0f1d
Alex Ho <alex.the.ho@gmail.com>
2022-09-23 18:53:47 -0400

    Added index.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     apps: [{
         port: 3000,
         name: "app",
-        script: "index.cjs",
+        script: "./index.cjs",
         watch: true,
         exec_mode: 'fork',
     }]
